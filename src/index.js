b5331934adfa3db1c76e401a9d708ff7a4a4afb1
Gopal Rohra <gopal.rohra@gmail.com>
2020-07-26 15:55:01 +0530

    Added import export for useInputFocus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import {useSSO} from './hooksso';
+import useInputFocus from './useInputFocus';
 import {MySpinner, Header, NotFound} from './components';
-export {useSSO, MySpinner, Header, NotFound};
+export {useSSO, MySpinner, Header, NotFound, useInputFocus};
